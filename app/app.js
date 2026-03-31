975f129a2b868012ab0a110dc4825370c2479fed
JoseManuelDuarteP <josduaper@alu.edu.gva.es>
2026-03-31 13:52:48 +0200

    LocalStorage funcional, Parseo de JSON parcial

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-function cargarJSON() {
+function cargarJSON(local) {
     let archivo = this.files[0];
     if (!archivo) return;
 
@@ -11,8 +11,13 @@ function leerJSON(archivo, callback) {
     let reader = new FileReader();
 
     reader.onload = function(event) {
+        let texto = event.target.result;
+
+        // Almacenar en navegador
+        localStorage.setItem("personajeJSON", texto);
+
         try {
-            let datos = JSON.parse(event.target.result);
+            let datos = JSON.parse(texto);
             callback(datos);
         } catch (e) {
             console.log("JSON no válido");
@@ -26,9 +31,31 @@ function leerJSON(archivo, callback) {
 function ponerJSONenElHTML(datos) {
     if (!datos) return;
 
-    document.getElementById("a").textContent += datos.a;
+    document.getElementById("skill-list").innerHTML = crearListaHTML(datos.habilidades);
+    document.getElementById("inventory-list").innerHTML = crearListaHTML(datos.inventario);
+    document.getElementById("mission-list").innerHTML +=
+        datos.misiones.map(mision =>
+            `<div class="mission">
+                <h5>${mision.nombre}</h5>
+                <p>${mision.detalles}</p>
+            </div>`).join('');
+    document.getElementById("persona-skill-list").innerHTML = crearListaHTML(datos.persona.habilidades);
+}
+
+function crearListaHTML(lista) {
+    return lista.map(x => `<li>${x}</li>`).join('');
+}
+
+function cargarJSONDesdeLocalStorage() {
+    let texto = localStorage.getItem("personajeJSON");
+    if (!texto) return;
+
+    let datos = JSON.parse(texto);
+    ponerJSONenElHTML(datos);
 }
 
 window.onload = function () {
-    document.getElementById("import").onchange = cargarJSON;
+    cargarJSONDesdeLocalStorage();
+
+    document.getElementById("fileField").onchange = cargarJSON;
 }
